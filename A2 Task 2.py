13a451b3ebd4f68360987be52c015fb6cbbbd448
SakshiT <tambekarsakshi09@gmail.com>
2025-12-15 15:49:25 +0530

    Add files via upload

---
+++ A2 Task 2.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Using for loop to iterate from 1 to 50
+for i in range(1, 51):
+    total_sum += i
+
+# Displaying the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
